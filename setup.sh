7a1535988dd647a90506e02e60c612503ac9d175
Uwe Kallmeyer <uwe.kallmeyer@gmail.com>
2022-04-17 11:50:39 +0200

    xx

--- setup.sh
+++ setup.sh
@@ -1,24 +1,33 @@
+#!/bin/bash
+
 # als root (su):
-#   apt install sudo
+#   apt install sudo git
 #   sudo adduser uwek sudo
 #   (user ab/anmelden)
 
-# sudo apt-get update
-# sudo apt-get upgrade
+function main() {
+sudo apt-get update
+sudo apt-get upgrade
 
-# sudo apt install git stow lightdm
-# sudo apt install i3 kitty
-# sudo apt install firefox-esr
+git config --global credential.helper store
+git config --global user.email "uwe.kallmeyer@gmail.com"
+git config --global user.name "Uwe Kallmeyer"
 
+sudo apt install stow lightdm i3 kitty firefox-esr
+sudo systemctl enable lightdm
+sudo systemctl set-default graphical.target
 
-# sudo systemctl enable lightdm
-# sudo systemctl set-default graphical.target
-
-# sudo vi /etc/lightdm/lightdm.conf
 # sudo sed -i 's|^# [Seat:\*].*$|[Seat:*]|g' /etc/lightdm/lightdm.conf
 sudo sed -i 's|^# autologin-user =.*$|autologin-user = uwek|g' /etc/lightdm/lightdm.conf
 sudo sed -i 's|^# autologin-user-timeout =.*$|autologin-user-timeout = 0|g' /etc/lightdm/lightdm.conf
 sudo sed -i 's|^# autologin-session =.*$|autologin-session = i3|g' /etc/lightdm/lightdm.conf
-# autologin etc.
 
+}
+
+
+function nomain() {
+echo "No Main"
+}
+
+nomain
 
