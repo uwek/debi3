6de68eb9b308c482d873496919a6707a9a9544ac
Uwe Kallmeyer <uwe.kallmeyer@gmail.com>
2022-04-17 12:39:12 +0200

    xx

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 # als root (su):
 #   apt install sudo git
 #   sudo adduser uwek sudo
+#   git clone https://github.com/uwek/debi3
 #   (user ab/anmelden)
 
-function main() {
+function s_basic() {
 sudo apt-get update
 sudo apt-get upgrade
 
@@ -13,21 +14,26 @@ git config --global credential.helper store
 git config --global user.email "uwe.kallmeyer@gmail.com"
 git config --global user.name "Uwe Kallmeyer"
 
-sudo apt install stow lightdm i3 kitty firefox-esr
+sudo apt install vim stow lightdm i3 kitty firefox-esr
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
-
-# sudo sed -i 's|^# [Seat:\*].*$|[Seat:*]|g' /etc/lightdm/lightdm.conf
-sudo sed -i 's|^# autologin-user =.*$|autologin-user = uwek|g' /etc/lightdm/lightdm.conf
-sudo sed -i 's|^# autologin-user-timeout =.*$|autologin-user-timeout = 0|g' /etc/lightdm/lightdm.conf
-sudo sed -i 's|^# autologin-session =.*$|autologin-session = i3|g' /etc/lightdm/lightdm.conf
-
 }
 
+function s_lightdm() {
+	echo "n.u."
+### manuell erledigt
+# sudo sed -i 's|^# [Seat:\*].*$|[Seat:*]|g' /etc/lightdm/lightdm.conf
+# sudo sed -i 's|^# autologin-user =.*$|autologin-user = uwek|g' /etc/lightdm/lightdm.conf
+# sudo sed -i 's|^# autologin-user-timeout =.*$|autologin-user-timeout = 0|g' /etc/lightdm/lightdm.conf
+# sudo sed -i 's|^# autologin-session =.*$|autologin-session = i3|g' /etc/lightdm/lightdm.conf
+}
 
 function nomain() {
 echo "No Main"
 }
 
-nomain
+# main
+
+git config --global user.email "uwe.kallmeyer@gmail.com"
+git config --global user.name "Uwe Kallmeyer"
 
